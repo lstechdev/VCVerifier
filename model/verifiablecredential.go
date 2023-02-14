dc6dfa69d52f99fd1add4ed39ef0cf6160f32f39
Stefan Wiedemann <wistefan@googlemail.com>
2023-02-14 14:38:10 +0100

    just take it as it is

--- model/verifiablecredential.go
+++ model/verifiablecredential.go
@@ -1,29 +1,5 @@
 package model
 
-type VerifiableCredential struct {
-	Context           []string               `json:"@context,omitempty"`
-	Id                string                 `json:"id,omitempty"`
-	Type              []string               `json:"type,omitempty"`
-	Issuer            string                 `json:"issuer,omitempty"`
-	IssuanceDate      string                 `json:"issuanceDate,omitempty"`
-	ValidFrom         string                 `json:"validFrom,omitempty"`
-	ExpirationDate    string                 `json:"expirationDate,omitempty"`
-	CredentialSubject CredentialSubject      `json:"credentialSubject,omitempty"`
-	Proof             map[string]interface{} `json:"proof"`
-}
-
-type Role struct {
-	// name of the role, for example READER
-	Names    []string `json:"names,omitempty"`
-	Target   string   `json:"target,omitempty"`
-	Provider string   `json:"provider,omitempty"`
-}
-
-type CredentialSubject struct {
-	Id    string `json:"id,omitempty"`
-	Roles []Role `json:"roles,omitempty"`
-}
-
 type Policy struct {
 	Policy   string       `json:"policy"`
 	Argument *TirArgument `json:"argument"`
--- verifier.go
+++ verifier.go
@@ -184,7 +184,7 @@ func (v *Verifier) VerifierAPIAuthenticationResponseVP(c *fiber.Ctx) error {
 
 func (v *Verifier) verifyCredential(credential []byte) (result bool, err error) {
 
-	var vcToVerify model.VerifiableCredential
+	var vcToVerify map[string]interface{}
 
 	json.Unmarshal(credential, &vcToVerify)
 
