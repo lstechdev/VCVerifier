17e0a812f3ea7d9fa992637746e1dc66d53adac1
Stefan Wiedemann <wistefan@googlemail.com>
2023-02-14 14:20:21 +0100

    add the proof

--- model/verifiablecredential.go
+++ model/verifiablecredential.go
@@ -1,14 +1,15 @@
 package model
 
 type VerifiableCredential struct {
-	Context           []string          `json:"@context,omitempty"`
-	Id                string            `json:"id,omitempty"`
-	Type              []string          `json:"type,omitempty"`
-	Issuer            string            `json:"issuer,omitempty"`
-	IssuanceDate      string            `json:"issuanceDate,omitempty"`
-	ValidFrom         string            `json:"validFrom,omitempty"`
-	ExpirationDate    string            `json:"expirationDate,omitempty"`
-	CredentialSubject CredentialSubject `json:"credentialSubject,omitempty"`
+	Context           []string               `json:"@context,omitempty"`
+	Id                string                 `json:"id,omitempty"`
+	Type              []string               `json:"type,omitempty"`
+	Issuer            string                 `json:"issuer,omitempty"`
+	IssuanceDate      string                 `json:"issuanceDate,omitempty"`
+	ValidFrom         string                 `json:"validFrom,omitempty"`
+	ExpirationDate    string                 `json:"expirationDate,omitempty"`
+	CredentialSubject CredentialSubject      `json:"credentialSubject,omitempty"`
+	Proof             map[string]interface{} `json:"proof"`
 }
 
 type Role struct {
