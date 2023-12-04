fc5eb468e5faf4265339e0a8c49dbe393ea352a8
Stefan Wiedemann <wistefan@googlemail.com>
2023-12-04 14:34:15 +0100

    try

--- tir/tokenProvider.go
+++ tir/tokenProvider.go
@@ -137,7 +137,7 @@ func (tp M2MTokenProvider) signVerifiablePresentation(authCredential *verifiable
 	err = vp.AddLinkedDataProof(&verifiable.LinkedDataProofContext{
 		Created:                 &created,
 		SignatureType:           "JsonWebSignature2020",
-		KeyType:                 kms.RSARS256Type,
+		KeyType:                 kms.RSAPS256Type,
 		ProofCreator:            proofCreator,
 		SignatureRepresentation: verifiable.SignatureJWS,
 		VerificationMethod:      tp.verificationMethod,
